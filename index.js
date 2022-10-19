1af267bc6cb518f54b7e7149eed4643ae4cd1c94
Anton Kulchytskyi <antek1809@gmail.com>
2022-10-19 22:21:59 +0200

    check

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-mongoose.connect('mongodb+srv://mongouser:q1w9e7r9t@cluster0.arjfndx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+// mongoose.connect('mongodb+srv://mongouser:q1w9e7r9t@cluster0.arjfndx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// );
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-    console.log("Connected successfully");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//     console.log("Connected successfully");
+// });
 
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// // Configuring body parser middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.get('/details', async (req, res) => {
-    // const user = await userModel.findOne({id: 123});
-    const user = await userModel.find({});
-    res.send(user);
-});
+// app.get('/details', async (req, res) => {
+//     // const user = await userModel.findOne({id: 123});
+//     const user = await userModel.find({});
+//     res.send(user);
+// });
 
 app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT}!`))
